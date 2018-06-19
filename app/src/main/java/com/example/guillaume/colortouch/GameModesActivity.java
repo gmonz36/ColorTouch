bdcd2702e8e1fe627a858c7d34f2ad8cf437ff20
Guillaume <gmonzerol@live.fr>
2018-06-18 21:03:10 -0400

    added a few layouts

---
+++ app/src/main/java/com/example/guillaume/colortouch/GameModesActivity.java
@@ -0,0 +1,13 @@
+package com.example.guillaume.colortouch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameModesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_modes);
+    }
+}
